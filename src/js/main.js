c3610edf42d2227ff61966c9aaee94efd6c10474
hoangviethung <hoangviet.hung.009@gmail.com>
2019-08-20 15:12:08 +0700

    60% TRANG CHỦ

--- src/js/main.js
+++ src/js/main.js
@@ -9,31 +9,73 @@ const addClassLazyload = () => {
 		}
 	});
 }
+// SLIDER BANNER
 function homeSliderBanner() {
 	var swpier = new Swiper('.slider-HomeBanner', {
-		effect: 'coverflow',
+		effect: 'fade',
+		fadeEffect: {
+			crossFade: true,
+		},
 		centeredSlides: true,
 		speed: 1000,
-		coverflowEffect: {
-			rotate: 50,
-			stretch: 0,
-			depth: 100,
-			modifier: 1,
-			slideShadows: true,
-		},
+		spaceBetween: 30,
 		loop: true,
-		// autoplay: true,
+		autoplay: true,
 		pagination: {
-			el: '.swiper-pagination',
+			el: '.slider-HomeBanner .swiper-pagination',
 			clickable: true,
 		},
 	});
 }
+// SLIDER PRODUCT
+function productSlider() {
+	var swiper = new Swiper(".product-slider .swiper-container", {
+		slidesPerView: 5,
+		loop: true,
+		speed: 1200,
+		autoplay: true,
+		pagination: {
+			el: '.product-slider .swiper-pagination',
+			clickable: true,
+		},
+		navigation: {
+			nextEl: '.product-slider .swiper-button-next',
+			prevEl: '.product-slider .swiper-button-prev',
+		},
+	})
+}
+// SLIDER BRAND
+function brandSlider() {
+	var swiper = new Swiper(".brand-slider .swiper-container", {
+		slidesPerView: 8,
+		loop: true,
+		speed: 1200,
+		autoplay: true,
+		pagination: {
+			el: '.brand-slider .swiper-pagination',
+			clickable: true,
+		},
+		navigation: {
+			nextEl: '.brand-slider .swiper-button-next',
+			prevEl: '.brand-slider .swiper-button-prev',
+		},
+	})
+}
+
+
+
+
+
+
+
+
 
 $(document).ready(function () {
 	objectFitImages("img.ofc"); // Luôn luôn chậy polyfill cho thuôc tính object-fit: cover trên các phiên bản IE >= 9
 	addClassLazyload(); // Luôn luôn addClass lazyload cho các hình ảnh có thuộc tính [data-src]
 	homeSliderBanner();
+	productSlider();
+	brandSlider();
 })
 
 $(document).ajaxComplete(function () {
